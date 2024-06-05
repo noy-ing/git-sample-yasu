eba4059e054ec1ba9d78985976f5269dc1237508
noy-ing <naoya20011102@icloud.com>
2024-06-05 10:55:51 +0900

    demo

---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,30 @@
+package com.example.domain;
+
+public class Car {
+    private Integer speed;
+    private String type;
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "speed=" + speed +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
